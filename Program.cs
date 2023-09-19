98306c0850c6ae1885abe332148d3f7860a6734a
Wertipol <barsukzak@gmail.com>
2023-09-19 19:17:55 +0700

    update

--- Program.cs
+++ Program.cs
@@ -6,44 +6,67 @@ namespace cs
     {
         static void Main(string[] args)
         {
-            Random rm = new Random();
-            Console.Write("Что надо вывести? \n1 - таблица умножение\n2 - учить таблицу умножения\n");
-            int action = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            if (action == 1)
+            while (true)
             {
-                for (int i = 1; i < 9; i++)
+                try
                 {
-                    for (int j = 1; j < 9; j++)
-                    {
-                        Console.Write($"{j} x {i} = {i * j} |");
-                    }
-                    Console.WriteLine();
-                }
-                Console.ReadLine();
-            }
+                    bool learning = true;
 
-            else if (action == 2)
-            {
-                Console.WriteLine("0 - Что бы выйти");
-                while (true)
-                {
-                    int a = rm.Next(1, 10);
-                    int b = rm.Next(1, 10);
-                    int c = a * b;
-                    int us = 0;
+                    Console.Write("Что надо сделать? \n0 - Что бы выйти\n1 - Вывести таблицу умножения\n2 - Учить таблицу умножения\n");
+                    int action = Convert.ToInt32(Console.ReadLine());
 
-                    while (us != c)
+                    Console.Clear();
+
+                    if (action == 1)
                     {
-                        Console.Write($"{a} x {b} = ");
-                        us = Convert.ToInt32(Console.ReadLine());
-                        if (us == 0)
+                        for (int i = 1; i < 9; i++)
                         {
-                            return;
+                            for (int j = 1; j < 9; j++)
+                            {
+                                Console.Write($"{j} x {i} = {i * j} |");
+                            }
+                            Console.WriteLine();
+                        }
+                        Console.ReadLine();
+                    }
+
+                    else if (action == 2)
+                    {
+                        Console.WriteLine("0 - Что бы выйти");
+
+                        while (learning)
+                        {
+                            learning = learn();
                         }
                     }
-                    Console.Clear();
+                    else { return; }
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            static bool learn()
+            {
+                Random rm = new Random();
+                int a = rm.Next(1, 10);
+                int b = rm.Next(1, 10);
+                int c = a * b;
+                int userInput = 0;
+
+                while (userInput != c)
+                {
+                    Console.Write($"{a} x {b} = ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInput == 0)
+                    {
+                        return false;
+                    }
+                }
+                Console.Clear();
+                return true;
             }
         }
     }
